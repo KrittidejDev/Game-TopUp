e32d180f22078b1e3702e81f21b838a3d3f55e6e
Jue Parinthon <jue.prt@hotmail.com>
2025-08-06 01:52:55 +0700

    feat: add GameInfoCard component and update CardsWidget layout

--- src/widgets/CardsWidget/index.js
+++ src/widgets/CardsWidget/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { CardsWidgetContainer } from "./styled";
+import { CardsWidgetContainer, GameInfoCard } from "./styled";
 
 const CardsWidget = () => {
   const [_filterBtnActice, _setFilterBtnActive] = useState(1);
@@ -28,7 +28,20 @@ const CardsWidget = () => {
           </div>
         </div>
       </div>
-      {_filterBtnActice === 1 && <div className="card_wrap">111111111111</div>}
+      {_filterBtnActice === 1 && <div className="card_wrap">
+        
+        <CardMockup title="Mobile Legend A" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Mobile Legend B" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Free Fire B" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Arena of Valor" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Gensin Impact" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="PUBG" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Call of Duty" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Tower Of Fantasy" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Lakapala" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        <CardMockup title="Game Lainnya" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." link="See more" />
+        
+        </div>}
 
       {_filterBtnActice === 2 && (
         <div className="card_wrap">2222222222222222222222</div>
@@ -42,3 +55,16 @@ const CardsWidget = () => {
 };
 
 export default CardsWidget;
+
+function CardMockup(props) {
+  return (
+    <div>
+      <GameInfoCard>
+      <div className="imgMockup"></div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <a href="#">{props.link}</a>
+      </GameInfoCard>
+      </div>
+  );
+}
--- src/widgets/CardsWidget/styled.js
+++ src/widgets/CardsWidget/styled.js
@@ -23,4 +23,36 @@ export const CardsWidgetContainer = styled.div`
       }
     }
   }
+    .card_wrap {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+`;
+
+export const GameInfoCard = styled.div `
+  width: 12.5rem;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 8px;
+  margin-top: 20px;
+    .imgMockup {
+      width: 200px;
+      height: 200px;
+      background-color: #ffffff; 
+      border-radius: 30px;
+    }
+    h2 {
+      color: #999999;
+      font-weight: 500;
+      margin-top: 10px;
+    }
+    p {
+      color: #4B4B4B;
+    }
+    a {
+      color: #C0621B;
+      font-size: 16px;
+    }
 `;
