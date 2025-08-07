da8919a1eaf80ce604b6459c5006ce22a6d08031
PTAE43 <kritsada1696@gmail.com>
2025-08-07 12:34:02 +0700

    feat: css /Card/Game

---
+++ src/components/Cards/Game/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const CardsGameContainer = styled.div`
+.group_card{
+    font-family: Poppins;
+    width: 258px;
+    leading-trim: NONE;
+    vertical-align: middle;
+    line-height: 3rem;
+    padding-top: 80px;
+    .images_card{
+        .images_game{
+            width: 258px;
+            height: 260px;}}
+    .title_card{
+        font-weight: 600;
+        font-style: SemiBold;
+        font-size: 26px;
+        letter-spacing: 0%;
+        color: #999999;}
+    .description_card{
+        font-weight: 500;
+        font-style: Medium;
+        font-size: 22px;
+        line-height: 2rem;
+        letter-spacing: 0%;
+        color: #4B4B4B; }
+    .readmore_card{
+        font-weight: 600;
+        font-style: SemiBold;
+        font-size: 22px;
+        letter-spacing: 0%;
+        color: #C0621B;}
+    .readmore_card img{
+        margin-bottom: -5px;
+        padding-left: 15px;}
+}
+`;
