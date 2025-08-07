ca591b1b14d3e46b7fed514c90998db1556ed785
PTAE43 <kritsada1696@gmail.com>
2025-08-07 18:14:50 +0700

    handling. - Created UI components for Select functionality using Radix UI. - Updated package.json to include @radix-ui/react-select dependency.

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,7 +1,10 @@
 // import BgStandard from "./BgStandard";
 
+import CardsGame from "./Game";
+
 const EXPORT = {
 //   BgStandard,
+    CardsGame,
 };
 
 export default EXPORT;
--- src/styles/HomeStyled.js
+++ src/styles/HomeStyled.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
 export const HomeStyled = styled.div`
-  padding: 80px 40px;
-  max-width: ${({ theme }) => theme.ETC.BODY_CONTAINER};
+  padding: 80px 100px;
 `;
