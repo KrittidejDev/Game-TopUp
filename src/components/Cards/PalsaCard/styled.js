435490fa4514a1a7be4f3f9c78c2b01bbc260386
PHET45 <phnjk2000@gmail.com>
2025-08-07 20:41:10 +0700

    feat: add PalsaCard component and integrate into CardsWidget

---
+++ src/components/Cards/PalsaCard/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PalsaContainer = styled.div`
+    width: 318;
+    height: 136;
+    angle: 0 deg;
+    opacity: 1;
+    top: 1475px;
+    left: 100px;
+    border-radius: 20px;
+    border-width: 2.5px;
+`
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,10 +1,12 @@
 // import BgStandard from "./BgStandard";
 
 import CardsGame from "./Game";
+import PalsaCard from "./PalsaCard"
 
 const EXPORT = {
 //   BgStandard,
     CardsGame,
+    PalsaCard,
 };
 
 export default EXPORT;
