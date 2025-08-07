723d6b62cc80ba6782765af4369e7fa6fbab13d2
PHET45 <phnjk2000@gmail.com>
2025-08-07 20:45:37 +0700

    refactor: encapsulate PalsaCard styles within a nested class for better structure

--- src/components/Cards/PalsaCard/styled.js
+++ src/components/Cards/PalsaCard/styled.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const PalsaContainer = styled.div`
-    width: 318;
-    height: 136;
-    angle: 0 deg;
-    opacity: 1;
-    top: 1475px;
-    left: 100px;
-    border-radius: 20px;
-    border-width: 2.5px;
+    .images_card{
+                width: 318;
+                height: 136;
+                angle: 0 deg;
+                opacity: 1;
+                top: 1475px;
+                left: 100px;
+                border-radius: 20px;
+                border-width: 2.5px;
+    }
 `
