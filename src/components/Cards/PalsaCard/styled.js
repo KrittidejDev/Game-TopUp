51187b6723db030e8123ce7736d7c8d68fb3508a
PHET45 <phnjk2000@gmail.com>
2025-08-10 00:46:50 +0700

    fix: correct title casing in PalsaCard mock data and enhance layout with additional styling adjustments

--- src/components/Cards/PalsaCard/styled.js
+++ src/components/Cards/PalsaCard/styled.js
@@ -2,19 +2,53 @@ import styled from "styled-components";
 
 export const PalsaContainer = styled.div`
     .container_palsa{
-                display: flex;
-                flex-diraction: row
-                width: 318;
-                height: 136;
-                angle: 0 deg;
-                opacity: 1;
-                top: 1475px;
-                left: 100px;
-                border-radius: 20px;
-                border-width: 2.5px;
+        border: 2.5px solid hsla(0, 0%, 29%, 1);
+        border-radius: 12px;
+        position: relative;
+        width: 318px;
+        height: 136px;
+
+    .box_card{
+        position: absolute; 
+        top: 20px;
+        display: flex;
+        flex-direction: row
+        justify-content: center; 
+        align-items: center;
+        margin-left: 35px;
+        margin-right: 35px;
+        
+    .image_card{
+        width: 100%;
+        height: 180px;
+        
+  
+    .image_game{
+        width: 100%;
+        height: 100%;
+        }
+        }
         .container_card{
-                display: flex;
-                flex-direction: column;
+               padding: 16px;
+               display: flex;
+               flex-direction: column;
+
+
+        .title_card{
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                font-size: 19px;
+                line-height: 1;
+                letter-spacing: 0;
+                                }
+        .description_card{
+                font-family: 'Poppins', sans-serif;
+                font-weight: 600;          
+                font-size: 32px;
+                line-height: 1;           
+                letter-spacing: 0;
+                            }
+            }
         }
     }
 `
