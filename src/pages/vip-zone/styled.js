760052045fc876ec8f14c908783be65e05f1d9db
PHET45 <phnjk2000@gmail.com>
2025-08-11 22:02:12 +0700

    feat: add terms and conditions to VipZone component and refactor layout for improved readability

--- src/pages/vip-zone/styled.js
+++ src/pages/vip-zone/styled.js
@@ -3,4 +3,12 @@ import styled from "styled-components";
 export const VipZoneContainer = styled.div`
   padding: 80px 20px;
   max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  items-align: center;
+  .vip-zone-banner{
+    width: 100%;
+    
+  }
 `;
